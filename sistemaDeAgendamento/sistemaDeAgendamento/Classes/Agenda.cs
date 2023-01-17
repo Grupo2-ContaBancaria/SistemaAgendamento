5d19666864073591c31221661727ab08d91c7293
monica88lima <flima.monica@gmail.com>
2023-01-17 16:35:01 -0300

    commit classe e metodo calendario propriedades da classe agenda

--- sistemaDeAgendamento/sistemaDeAgendamento/Classes/Agenda.cs
+++ sistemaDeAgendamento/sistemaDeAgendamento/Classes/Agenda.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 
 namespace sistemaDeAgendamento.Classes
 {
-    public class Agenda:Base
+    public class Agenda : Base
     {
         //Eu Monica vou criar a classe agenda - inicialemnte vou definir as propriedades e o tipo de dado de cada propriedade
-        public string Especialidade { get; set; } = "";
-        public string NomeMedico { get; set; }
+        public string Nome_Usuario { get; set; }
+        public string Nome_Medico { get; set; }
+        public string Nome_Especialidade { get; set; }
+        public string Nome_Procedimento { get; set; }
+        public string Nome_FormaPgto { get; set; }
+        public DateTime Data_Marcada { get; set; }
+        public DateTime Hora_Marcada { get; set; }
     }
 }
---
+++ sistemaDeAgendamento/sistemaDeAgendamento/Metodos/Calendario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaDeAgendamento.Metodos
+{
+    public class Calendario
+    {
+        public List<Classes.Calendario> LstCalendarios { get; set; }
+
+        public Calendario()
+        {
+            var hora = 8;
+            var data = 1;
+            var random = new Random();
+
+            // o 15 representa a quantidade de medicos cadastrados, preciso alterar quandoo tiver a qdt exata
+            for (int i = 1; i <= 15; i++)
+            {
+                if (hora > 16)
+                {
+                    hora = 8;
+                    data++;
+                }
+
+                LstCalendarios.Add(
+                    new Classes.Calendario()
+                    {
+                        Id = i,
+                        Data = DateTime.Now.AddDays(data).ToString("dd/MM/YYYY"),
+                        Hora = new DateTime(2023, 12, 1, hora, 0, 0).AddHours(i).ToString("HH:mm"),
+                        Situacao = "l",
+                        MedicoId = random.Next(1, 42)
+                    });
+
+                hora++;
+
+            }
+        }
+    }
+}
