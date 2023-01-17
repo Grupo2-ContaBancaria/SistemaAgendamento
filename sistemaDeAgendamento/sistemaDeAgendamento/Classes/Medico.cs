a0512536d03515cd6ab1ffb2e60a6f4669adf81c
monica88lima <flima.monica@gmail.com>
2023-01-17 18:32:30 -0300

    commit classe medico

---
+++ sistemaDeAgendamento/sistemaDeAgendamento/Classes/Medico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaDeAgendamento.Classes
+{
+    public class Medico: Base
+    {
+        public int Id { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+
+        public bool Status { get; set; }
+
+        // cada médico tem sua especialidade
+        public string especialidade { get; set; }
+
+    }
+}
