c6c70b7fece2ac6e1d5bea211376912967d5972c
Rodrigo <rodrigo.freire@msn.com>
2023-01-18 12:05:46 -0300

    Adicionando o método Ligar no Main

--- sistemaDeAgendamento/sistemaDeAgendamento/Classes/Paciente.cs
+++ sistemaDeAgendamento/sistemaDeAgendamento/Classes/Paciente.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace sistemaDeAgendamento.Classes
 {
+    //Eu Rodrigo-Freire vou criar a Class Paciente
     public class Paciente
     {
+        //Definindo as propriedades para a Class
+        public long cpf { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+
+        //criando método para validar cpf (ainda ta bem no começo, mas vou conseguir desenvolver! oremos! kkkk)
+        public bool checkCpf(string cpf)
+        {
+            if (cpf.Length != 11)
+            {
+                Console.WriteLine("CPF invalido!" +
+                    "\nPor favor, digite novamente");
+                return false;
+            }
+            return true;
+        }
     }
 }
